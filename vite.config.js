fd807d4875ff17d9a84169d26e4733bb8ed5e6e3
varuntv97 <varun_tv@outlook.com>
2024-08-01 15:10:20 +0530

    chore: Add rollup-plugin-visualizer for bundle analysis

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), visualizer()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('vuetify')) {
+              return 'vuetify'
+            }
+            return 'vendor'
+          }
+        }
+      }
+    }
+  }
 })
